d8c9da9c7c36a01d53a50af1ae965bfdc299574b
Donald <donumeh54@gmail.com>
2024-10-04 23:01:09 +0100

    handle multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const { body } = responses[0];
+      const { firstName, lastName } = responses[1];
+
+      console.log(body, firstName, lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
