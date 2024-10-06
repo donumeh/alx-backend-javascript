51fe3fe48c9679634d382652217ccd4b727c0e33
Donald <donumeh54@gmail.com>
2024-10-07 00:18:27 +0100

    final user creation for task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signupPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-    const signupPromise = signUpUser(firstName, lastName);
-    const uploadPhotoPromise = uploadPhoto(fileName);
-
-
-    return Promise.allSettled([signupPromise, uploadPhotoPromise])
-            .then(responses => responses.map((res) => ({
-                status: res.status,
-                value: res.status === 'fulfilled' ? res.value : res.reason,
-            })));
+  return Promise.allSettled([signupPromise, uploadPhotoPromise])
+    .then((responses) => responses.map((res) => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : `${res.reason}`,
+    })));
 }
