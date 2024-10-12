e337e9da47d97d3df0c43652cbedefeca60ee83d
Donald <donumeh54@gmail.com>
2024-10-12 09:14:45 +0100

    update uniq items in map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(map) {
-  if (!map && map instanceof Map) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
 
   for (const [key, value] of map) {
-    if (typeof value === 'number' && value === 1) {
-      map.set(key, value * 100);
+    if (value === 1) {
+      map.set(key, 100);
     }
   }
 }
