32a5e940be36145bf4f1f2c49c580487256d02cc
Donald <donumeh54@gmail.com>
2024-10-12 01:25:10 +0100

    filter students

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+export default function getStudentsByLocation(getListStudents, city) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+  if (!city) {
+    return getListStudents;
+  }
+
+  return getListStudents.filter((obj) => obj.location === city);
+}
