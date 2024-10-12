30bdea6e988f072ff6823a336aefdbcbf4e4e2c3
Donald <donumeh54@gmail.com>
2024-10-12 12:33:41 +0100

    weak map

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,15 @@
 export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
-  if (!(endpoint instanceof Object)) {
-    throw new Error('Endpoint load is high');
-  }
+    if (!endpoint && typeof endpoint !== 'object') {
+        throw new Error('Endpoint load is high');
+    }
 
-  const queryCount = weakMap.get(endpoint) || 0;
+    const queryCount = weakMap.get(endpoint) || 0;
 
-  if (queryCount >= 5) {
-    throw new Error('Endpoint load is high');
-  }
+    if (queryCount >= 5) {
+        throw new Error('Endpoint load is high');
+    }
 
-  weakMap.set(endpoint, queryCount + 1);
+    weakMap.set(endpoint, queryCount + 1);
 };
