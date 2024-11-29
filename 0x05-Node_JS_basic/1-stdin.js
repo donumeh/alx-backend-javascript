8f6ae730c9a54dd21a1287f1d10569e1845c0cf2
Donald <donumeh54@gmail.com>
2024-11-29 15:29:09 +0100

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,23 @@
+
+
+const welcomeMessage = () => {
+    console.log('Welcome to Holberton School, what is your name?');
+
+    process.stdin.on('data', (input) => {
+        const name = input;
+        console.log(`Your name is: ${name}`);
+
+        console.log('This important software is now closing');
+
+        process.exit();
+    });
+
+    process.on('SIGINT', () => {
+        console.log('This important software is not closing');
+        process.exit();
+    });
+}
+
+
+welcomeMessage();
+
